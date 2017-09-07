61d72cd5cfb705137c6390a7dceab3728a8067bd
eobaah <eobaah@gmail.com>
2017-09-06 18:11:04 -0700

    add modal for edit

--- public/script.js
+++ public/script.js
@@ -12,8 +12,19 @@ $(document).ready(function(){
   })
 
   $(document).on('click', '.edit-btn', function()  {
-    let id = this.parentElement.getAttribute("data-id")
-    console.log(id)
+    let id = $(this.parentElement).data("id")
+    let artist = $(this.parentElement).data("artist")
+    let name = $(this.parentElement).data("name")
+    let releaseDate = $(this.parentElement).data("release")
+    let version = $(this.parentElement).data("version")
+    let genres = $(this.parentElement).data("genres")
+
+    $(".modal-title").html(`<h1>${id}</h1>`)
+    $("input[name=artist]").val(artist)
+    $("input[name=name]").val(name)
+    $("input[name=date]").val(releaseDate)
+    $("input[name=version]").val(version)
+    $("input[name=genre]").val(genres)
   })
 
   $(document).on('click', '.delete-btn', function() {
@@ -66,11 +77,18 @@ const getAllAlbums = () => {
     albums.forEach( album => {
       $(".list-group")
         .append(`
-        <li class="list-group-item" data-id="${album._id}"
-        data-toggle="modal" data-target="#exampleModal">
+        <li class="list-group-item"
+          data-id="${album._id}"
+          data-artist="${album.artistName}"
+          data-name="${album.name}"
+          data-release="${album.releaseDate}"
+          data-version="${album.__v}"
+          data-genres="${album.genres}"
+          >
         ${JSON.stringify(album.name)}
          <i class="fa fa-trash-o" aria-hidden="true"></i>
-         <a class="edit-btn"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+         <a class="edit-btn" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+        </li>
          `);
     })
   } )
